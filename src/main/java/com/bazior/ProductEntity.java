2f22d71085e1a43a67b489a2499afc5f637640e8
Andrzej Bazior <andrzej.bazior@eniro.com>
2016-09-10 23:31:27 +0200

    first query

--- src/main/java/com/bazior/ProductEntity.java
+++ src/main/java/com/bazior/ProductEntity.java
@@ -1,10 +1,16 @@
 package com.bazior;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
 @Builder
+@Document(indexName = "products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductEntity{
 
     private String id;
--- src/test/java/com/bazior/FtuApplicationTests.java
+++ src/test/java/com/bazior/FtuApplicationTests.java
@@ -5,19 +5,27 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FtuApplicationTests {
 
-    public static final String PRODUCT = "product";
+    public static final String PRODUCT = "products";
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
 
     private void indexDocument(){
+        elasticsearchTemplate.deleteIndex(PRODUCT);
         elasticsearchTemplate.createIndex(PRODUCT);
         ProductEntity product = ProductEntity
                 .builder()
@@ -35,12 +43,20 @@ public class FtuApplicationTests {
         elasticsearchTemplate.refresh(PRODUCT);
     }
 
+    private List<ProductEntity> query(String queryString){
+
+        Criteria criteria = new Criteria("name");
+        criteria.contains(queryString);
+        CriteriaQuery criteriaQuery = new CriteriaQuery(criteria);
+        return elasticsearchTemplate.queryForList(criteriaQuery, ProductEntity.class);
+    }
 
 	@Test
 	public void elasticConnectionTest() {
         indexDocument();
-
-
+        List<ProductEntity> list = query("foo");
+        assertNotNull(list);
+        assertEquals(1, list.size());
 	}
 
 }
